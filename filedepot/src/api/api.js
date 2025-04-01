bc8badbf640aa18b74c329341a855dbb14ae46bc
jreyess11 <josegabrielreyessolanojr@gmail.com>
2025-04-01 10:59:16 -0500

    Update api.js

--- filedepot/src/api/api.js
+++ filedepot/src/api/api.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
+import { useAuthStore } from '@/stores/authStore';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api', // Ajustar URL
-  headers: {
-    'Content-Type': 'application/json',
-  }
+  baseURL: 'http://localhost:3000/api',
+  headers: { 'Content-Type': 'application/json' }
 });
+
+// Interceptor para agregar el token a cada solicitud
+apiClient.interceptors.request.use((config) => {
+  const authStore = useAuthStore();
+  if (authStore.token) {
+    config.headers.Authorization = `Bearer ${authStore.token}`;
+  }
+  return config;
+}, error => Promise.reject(new Error(error)));
 
 export default apiClient;
